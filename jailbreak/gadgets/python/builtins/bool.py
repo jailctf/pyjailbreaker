d2562d9c87e8d15e9c4cd77fe6071b8ab534308b
Nico Posada <posadanico@outlook.com>
2024-08-13 22:26:50 -0500

    two more gadgets!!!!!

--- jailbreak/gadgets/python/builtins/bool.py
+++ jailbreak/gadgets/python/builtins/bool.py
@@ -5,5 +5,11 @@ Gadgets for obtaining the `bool` type.
 def bool__dunder_class():
     return True.__class__
 
-def bool__type():
+def bool__type(*, type):
     return type(True)
+
+def bool__dunder_class_num():
+    return (1<2).__class__
+
+def bool__type_num(*, type):
+    return type(1<2)
