31ef29621f4e7bcda6d33f640bccc15d6395395b
Quasar0147 <102931302+Quasar0147@users.noreply.github.com>
2024-09-17 22:54:09 -0600

    Update dict_getitem.py

--- jailbreak/gadgets/python/dict_getitem.py
+++ jailbreak/gadgets/python/dict_getitem.py
@@ -2,6 +2,13 @@
 Gadgets that work the same as subscripting a dictionary / `dict.__getitem__`.
 """
 
+# base case
+def dict_getitem__subscr(dict, key):
+    return dict[key]
+
+def dict_getitem__getitem(dict, key):
+    return dict.__getitem__(key)
+
 def dict_getitem__get(dict, key):
     return dict.get(key)
 
